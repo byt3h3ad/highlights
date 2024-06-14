64482629958196b64be3eed096b0ed21ee7e0903
byt3h3ad <adhirajd007@gmail.com>
2024-06-15 00:15:30 +0530

    feat: core implementation done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'unavatar.io',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
